371dd6987c99377135cc279375cecb4a65d8d3bf
Tomoya0126 <74522816+Tomoya0126@users.noreply.github.com>
2021-10-06 15:33:37 +0900

    Create home.js

---
+++ home.js
@@ -0,0 +1,11 @@
+(function() {
+    //btnxxxは各ゲームへ飛ぶボタンの要素
+    //足し算
+    const btnTashi = document.getElementById("btnTashi");
+    //かけ算
+    const btnKake = document.getElementById("btnKake");
+    //引き算
+    const btnHiki = document.getElementById("btnHiki");
+    //わり算
+    const btnWari = document.getElementById("btnWari");
+})()
